01913243877e482e5fcb5de9b12ec2bb06fdb199
Ram Rapolu <hiramrapolu@gmail.com>
2024-04-18 23:31:58 +0100

    adding mocha testing

---
+++ 4-testing-callbacks/calculate-square.mjs
@@ -0,0 +1,10 @@
+export function calculateSquare(number, callback) {
+    setTimeout(function() {
+        if (typeof number !== 'number') {
+            callback(new Error('Argument of type number is expected'));
+            return;
+        }
+        const result = number * number;
+        callback(null, result);
+    }, 1000);
+}
